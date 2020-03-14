0d2dc4e85f5aebf5764295b933c21cc57078727e
Ronaldo64 <devronaldo.costa@gmail.com>
2020-03-14 18:38:34 -0400

    Correção da classe Empresa e codificação das classes Desenvolvedor e Endereco

--- src/main/java/model/Desenvolvedor.java
+++ src/main/java/model/Desenvolvedor.java
@@ -1,9 +1,116 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * @author Ronaldo Costa
  *
  */
-public class Desenvolvedor {
+public class Desenvolvedor extends Usuario{
 
+	private String nome;
+	private String cpf;
+	private String dataNascimento;
+	private String tempoExperiencia;
+	private String linkedIn;
+	private String gitHub;
+	private ArrayList<DesenvolvedorAreaAtuacao> areasAtuacao;
+	private ArrayList<DesenvolvedorHabilidade> habilidades;
+	private String curriculo;
+	private ArrayList<Solucao> solucoes;
+	
+	public Desenvolvedor() {}
+
+	public Desenvolvedor(String email, String foto, String senha, Endereco endereco, String site, String telefone,
+			String apresentacao, String nome, String cpf, String dataNascimento, String tempoExperiencia,
+			String linkedIn, String gitHub, String curriculo) {
+		super(email, foto, senha, endereco, site, telefone, apresentacao);
+		this.nome = nome;
+		this.cpf = cpf;
+		this.dataNascimento = dataNascimento;
+		this.tempoExperiencia = tempoExperiencia;
+		this.linkedIn = linkedIn;
+		this.gitHub = gitHub;
+		this.curriculo = curriculo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getDataNascimento() {
+		return dataNascimento;
+	}
+
+	public void setDataNascimento(String dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+
+	public String getTempoExperiencia() {
+		return tempoExperiencia;
+	}
+
+	public void setTempoExperiencia(String tempoExperiencia) {
+		this.tempoExperiencia = tempoExperiencia;
+	}
+
+	public String getLinkedIn() {
+		return linkedIn;
+	}
+
+	public void setLinkedIn(String linkedIn) {
+		this.linkedIn = linkedIn;
+	}
+
+	public String getGitHub() {
+		return gitHub;
+	}
+
+	public void setGitHub(String gitHub) {
+		this.gitHub = gitHub;
+	}
+
+	public ArrayList<DesenvolvedorAreaAtuacao> getAreasAtuacao() {
+		return areasAtuacao;
+	}
+
+	public void setAreasAtuacao(ArrayList<DesenvolvedorAreaAtuacao> areasAtuacao) {
+		this.areasAtuacao = areasAtuacao;
+	}
+
+	public ArrayList<DesenvolvedorHabilidade> getHabilidades() {
+		return habilidades;
+	}
+
+	public void setHabilidades(ArrayList<DesenvolvedorHabilidade> habilidades) {
+		this.habilidades = habilidades;
+	}
+
+	public String getCurriculo() {
+		return curriculo;
+	}
+
+	public void setCurriculo(String curriculo) {
+		this.curriculo = curriculo;
+	}
+
+	public ArrayList<Solucao> getSolucoes() {
+		return solucoes;
+	}
+
+	public void setSolucoes(ArrayList<Solucao> solucoes) {
+		this.solucoes = solucoes;
+	}
 }
