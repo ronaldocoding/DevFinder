c75aeae494ea4b2a63992e544c8c48ab83dd119b
MarlonFL15 <marlonfleite50@gmail.com>
2020-03-15 12:22:03 -0400

    validação do cadastro da empresa e do desenvolvedor

--- src/main/webapp/habilidades.js
+++ src/main/webapp/habilidades.js
@@ -1,6 +1,7 @@
 let hab = ["C", "C++", "C#", "JavaScript", "MySQL", "Java", "Python"];
 let idSelect = "habilidades-select";
 let idTabela = "habilidades-tabela";
+var escolhido = 0;
 cria(hab);
 adEventos();
 function cria(){
@@ -27,19 +28,22 @@ function adiciona(index){
     if(index >= -1){
         var tab = document.getElementById(idTabela).getElementsByTagName("tr");
         tab[index].style.display="inline-block";
+        escolhido += 1;
     }
 }
 function deleta(index){
     if(index >= -1){
         var tab = document.getElementById(idTabela).getElementsByTagName("tr");
         tab[index].style.display="none";
+        escolhido -= 1;
     }
 }
 
-$('#habilidades-select').click(function(){
+$('#habilidades-select').change(function(){
     adiciona($('#habilidades-select').prop('selectedIndex')-1)
 });
 
 $('#habilidades-tabela').click(function(){
+    console.log('uau');
     deleta($('#habilidades-select').prop('selectedIndex'))
 });
