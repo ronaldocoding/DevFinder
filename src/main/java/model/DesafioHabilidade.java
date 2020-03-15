89d77846781973617119ec17839235a19901f555
Ronaldo64 <devronaldo.costa@gmail.com>
2020-03-14 21:45:16 -0400

    Codificação da classe DesafioHabilidade

--- src/main/java/model/DesafioHabilidade.java
+++ src/main/java/model/DesafioHabilidade.java
@@ -6,4 +6,30 @@ package model;
  */
 public class DesafioHabilidade {
 
+	private String emailEmpresa;
+	private int idDesafio;
+	private String habilidade;
+
+	public DesafioHabilidade() {
+	}
+
+	public DesafioHabilidade(String emailEmpresa, int idDesafio, String habilidade) {
+		super();
+		this.emailEmpresa = emailEmpresa;
+		this.idDesafio = idDesafio;
+		this.habilidade = habilidade;
+	}
+
+	public String getEmailEmpresa() {
+		return emailEmpresa;
+	}
+
+	public int getIdDesafio() {
+		return idDesafio;
+	}
+
+	public String getHabilidade() {
+		return habilidade;
+	}
+
 }
