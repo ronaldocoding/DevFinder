1b572f69da4af5e933f515428807a6570fb75c83
Ronaldo64 <devronaldo.costa@gmail.com>
2020-03-14 21:51:54 -0400

    Codificação da classe Solucao

--- src/main/java/model/Solucao.java
+++ src/main/java/model/Solucao.java
@@ -1,9 +1,57 @@
 package model;
 
 /**
- * @author ronal
+ * @author Ronaldo Costa
  *
  */
 public class Solucao {
+	
+	private String emailDesenvolvedor;
+	private String emailEmpresa;
+	private String idDesafio;
+	private String nome;
+	private String linkGithub;
+	private String documentacao;
+	private String descricao;
+	
+	public Solucao() {}
 
+	public Solucao(String emailDesenvolvedor, String emailEmpresa, String idDesafio, String nome, String linkGithub,
+			String documentacao, String descricao) {
+		this.emailDesenvolvedor = emailDesenvolvedor;
+		this.emailEmpresa = emailEmpresa;
+		this.idDesafio = idDesafio;
+		this.nome = nome;
+		this.linkGithub = linkGithub;
+		this.documentacao = documentacao;
+		this.descricao = descricao;
+	}
+
+	public String getEmailDesenvolvedor() {
+		return emailDesenvolvedor;
+	}
+
+	public String getEmailEmpresa() {
+		return emailEmpresa;
+	}
+
+	public String getIdDesafio() {
+		return idDesafio;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getLinkGithub() {
+		return linkGithub;
+	}
+
+	public String getDocumentacao() {
+		return documentacao;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
 }
