bd7f6c41435c6713ff0c85bb4602074c5cc72f2c
Ronaldo64 <devronaldo.costa@gmail.com>
2020-03-14 21:57:16 -0400

    Codificação da classe EmpresaNotificacao

--- src/main/java/model/EmpresaNotificacao.java
+++ src/main/java/model/EmpresaNotificacao.java
@@ -6,4 +6,35 @@ package model;
  */
 public class EmpresaNotificacao {
 
+	private String emailEmpresa;
+	private String data;
+	private String horario;
+	private String mensagem;
+
+	public EmpresaNotificacao() {
+	}
+
+	public EmpresaNotificacao(String emailEmpresa, String data, String horario, String mensagem) {
+		super();
+		this.emailEmpresa = emailEmpresa;
+		this.data = data;
+		this.horario = horario;
+		this.mensagem = mensagem;
+	}
+
+	public String getEmailEmpresa() {
+		return emailEmpresa;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public String getHorario() {
+		return horario;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
 }
