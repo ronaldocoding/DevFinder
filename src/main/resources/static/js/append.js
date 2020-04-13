a16409dc12d708a75c022af75e3627073cec7a89
Monike Freitas <monike.ftsousa@gmail.com>
2020-04-12 22:11:28 -0400

    Alteracoes no css

--- src/main/resources/static/js/append.js
+++ src/main/resources/static/js/append.js
@@ -2,18 +2,16 @@ var $footer = $('<footer class="container py-5 px-5"><div class="row">'+
                 '<div class="col-12 col-md text-center"><img class="img-fluid noturno" src="../static/imgs/logoNoturna.png"></div>'+
                 '<div class="col-6 col-md"><h5>Desenvolvedores</h5>'+
                 '<ul class="list-unstyled text-small">'+ 
-                '<li><a href="#">Erica Medeiros</a></li>'+
-                '<li><a href="#">Marlon Faria</a></li>'+
-                '<li><a href="#">Monike Freitas</a></li>'+
-                '<li><a href="#">Ronaldo Costa</a></li></ul></div>'+
+                '<li><a href="https://github.com/Erigoddess">Erica Medeiros</a></li>'+
+                '<li><a href="https://github.com/MarlonFL15">Marlon Faria</a></li>'+
+                '<li><a href="https://github.com/mnik3007">Monike Freitas</a></li>'+
+                '<li><a href="https://github.com/Ronaldo64">Ronaldo Costa</a></li></ul></div>'+
                 '<div class="col-6 col-md"><h5>Endereço</h5><ul class="list-unstyled text-small">'+
-                '<li><a href="#">Av. Sete de Setembro, 1975, IFAM, Manaus, AM - Brasil</a></li></ul></div>'+
+                '<li><a href="https://goo.gl/maps/hEkmvv2gVBFypU7g6">Av. Sete de Setembro, 1975, IFAM, Manaus, AM - Brasil</a></li></ul></div>'+
                 '<div class="col-6 col-md"><h5>Contato</h5><ul class="list-unstyled text-small">'+
                 '<li><a href="#">devfinder@gmail.com</a></li></ul></div></div></footer>');
 
-var $topnav=$('<script> var logo= function(){if($( window ).width()<=610){$(".logo").addClass("small");}else{$(".logo").removeClass("small");}};'+
-              '$(window).on("resize", logo);'+
-              '$(window).on("load", logo);</script>'+
+var $topnav=$('<script> var logo= function(){if($( window ).width()<=710){$(".logsdo").addClass("small");$(".buscar").hide();}else{$(".logo").removeClass("small");$(".buscar").show();}};$(window).on("resize", logo);$(window).on("load", logo);</script>'+
               '<div class="container demo"><div class="top_navbar-side"><div class="top_menu-side">'+
               '<ul><li><div class="hamburger-side btn-degrade azul" data-toggle="modal" data-target="#sidebar">'+
               '<div class="one"></div><div class="two"></div><div class="three"></div></div></li><li>'+
@@ -21,7 +19,7 @@ var $topnav=$('<script> var logo= function(){if($( window ).width()<=610){$(".lo
               '<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02"'+ 
               'aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">'+
               '<span class="navbar-toggler-icon"></span></button>'+
-              '<ul><li><div class="buscar-caja"><input type="text" name="" class="buscar-txt" placeholder="Buscar..."/>'+
+              '<ul><li class="buscar"><div class="buscar-caja"><input type="text" name="" class="buscar-txt" placeholder="Buscar..."/>'+
               '<a class="buscar-btn  btn-azul"><i class="fas fa-search"></i></a></div></li>'+
               '<li><div class="dropdown show"><a class="dropdown-togggle btn-degrade azul redondo" id="dropdownMenuLink"data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">'+
               '<i class="fa fa-bell"></i></a>'+
@@ -35,7 +33,7 @@ var $sidebar_dev=$('<div class="modal left fade" id="sidebar" tabindex="-1" role
               '<img src="../static/imgs/logo.png"  class="logo"></li></ul>'+
                    '<ul id="menu-list">'+
                    '<li><a href="devInicio.html"><span class="icon"><i class="fa fa-home" aria-hidden="true"></i></span><span class="title">Início</span></a></li>'+
-                   '<li><a href="devDesafiosInscritos.html"><span class="icon"><i class="fa fa-code"></i></span><span class="title">Desafios Inscritos</span></a></li><li><a href="#" class="active">'+
+                   '<li><a href="devDesafiosInscritos.html"><span class="icon"><i class="fa fa-code"></i></span><span class="title">Desafios Inscritos</span></a></li><li><a href="devConfiguracoes.html">'+
                    '<span class="icon"><i class="fa fa-cog" aria-hidden="true"></i></span>'+
                    '<span class="title">Configurações</span>'+
                    '</a></li><li><a href="homepage.html">'+
@@ -43,19 +41,33 @@ var $sidebar_dev=$('<div class="modal left fade" id="sidebar" tabindex="-1" role
                    '<span class="title">Sair</span>'+
                    '</a>'+
                    '</ul></div></div></div></div>');
-var $sidebar_emp=$('');
-var $defaultnav=$('<script> var logo= function(){if($( window ).width()<=690){$(".logo").addClass("small");}else{$(".logo").removeClass("small");}};$(window).on("resize", logo);$(window).on("load", logo);</script>'+
+var $sidebar_emp=$('<div class="modal left fade" id="sidebar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">'+
+                   '<div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-body">'+
+                   '<ul id="menu-nav"><li><div class="hamburger-side btn-degrade azul" data-toggle="modal" data-target="#sidebar">'+
+              '<div class="one"></div><div class="two"></div><div class="three"></div></div></li><li>'+
+              '<img src="../static/imgs/logo.png"  class="logo"></li></ul>'+
+                   '<ul id="menu-list">'+
+                   '<li><a href="empInicio.html"><span class="icon"><i class="fa fa-home" aria-hidden="true"></i></span><span class="title">Início</span></a></li>'+
+                   '<li><a href="empMeusDesafios.html"><span class="icon"><i class="fa fa-code"></i></span><span class="title">Meus desafios</span></a></li><li><a href="empConfiguracoes.html">'+
+                   '<span class="icon"><i class="fa fa-cog" aria-hidden="true"></i></span>'+
+                   '<span class="title">Configurações</span>'+
+                   '</a></li><li><a href="homepage.html">'+
+                   '<span class="icon"><i class="fas fa-sign-out-alt"></i></span>'+
+                   '<span class="title">Sair</span>'+
+                   '</a>'+
+                   '</ul></div></div></div></div>');
+var $defaultnav=$('<script> var logo= function(){if($( window ).width()<=690){$(".logsdo").addClass("small");$(".buscar").hide();}else{$(".logo").removeClass("small");$(".buscar").show();}};$(window).on("resize", logo);$(window).on("load", logo);</script>'+
                   '<nav class="top_navbar-side">'+
                   '<div class="top_menu-side">'+
-                  '<div><a href="homepage.html"><img src="../static/imgs/logo.png" class="logo"></a></div><ul><li>'+
+                  '<div><a href="homepage.html"><img src="../static/imgs/logo.png" class="logo"></a></div><ul><li class="buscar">'+
                   '<div class="buscar-caja"><input type="text" name="" class="buscar-txt" placeholder="Buscar..."/>'+
                   '<a class="buscar-btn"><i class="fas fa-search"></i></a></div></li>'+
                   '<li><a href="#linkLogin" data-toggle="modal" data-target="#modalExemplo" class="btn-azul">Entrar</a></li><li>'+
                   '<div class="dropdown show"><a class="dropdown-togggle btn-degrade azul" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Cadastrar</a>'+
-                  '<div class="dropdown-menu p-2 text-center" aria-labelledby="dropdownMenuLink"><h4>Empresa</h4><h4>Desenvolvedor</h4></div></div>'+
+                  '<div class="dropdown-menu p-2 text-center" aria-labelledby="dropdownMenuLink"><a href="formEmpresa.html"><h4>Empresa</h4></a><h4>Desenvolvedor</h4></div></div>'+
                   '</li></ul></div></nav>');
-
 $( ".footer" ).append($footer);
 $( ".topnav" ).append($topnav);
 $(".defaultnav").append($defaultnav);
 $(".sidebardev").append($sidebar_dev);
+$(".sidebaremp").append($sidebar_emp);
