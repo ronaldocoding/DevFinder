52cc65dcffd9684b60ea4b4866438f30749efa29
Ronaldo64 <devronaldo.costa@gmail.com>
2020-06-18 17:25:34 -0400

    Classe DesenvolvedorDesafio e adição de método findByDesafio na classe DesenvolvedorDesafioRepository

--- src/main/java/br/com/devfinder/repository/DesenvolvedorDesafioRepository.java
+++ src/main/java/br/com/devfinder/repository/DesenvolvedorDesafioRepository.java
@@ -1,10 +1,20 @@
 package br.com.devfinder.repository;
 
+import java.util.List;
+
+/**
+ * @author Ronaldo Costa
+ *
+ */
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.devfinder.model.DesenvolvedorDesafio;
 import br.com.devfinder.model.ids.DesenvolvedorDesafioId;
 
 public interface DesenvolvedorDesafioRepository extends JpaRepository<DesenvolvedorDesafio, DesenvolvedorDesafioId> {
+
+	@Query(value = "SELECT * FROM DESENVOLVEDOR_DESAFIO WHERE EMAIL_EMPRESA_DESAFIO = ?1 AND ID_DESAFIO = ?2", nativeQuery = true)
+	List<DesenvolvedorDesafio> findAllByDesafio(String emailEmpresa, int idDesafio);
 
 }
---
+++ src/main/java/br/com/devfinder/service/DesenvolvedorDesafioService.java
@@ -0,0 +1,48 @@
+package br.com.devfinder.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.com.devfinder.model.DesenvolvedorDesafio;
+import br.com.devfinder.model.ids.DesenvolvedorDesafioId;
+import br.com.devfinder.repository.DesenvolvedorDesafioRepository;
+
+/**
+ * @author Ronaldo Costa
+ *
+ */
+public class DesenvolvedorDesafioService {
+
+	@Autowired
+	private DesenvolvedorDesafioRepository repository;
+	
+	/**
+	 * Métodos POST
+	 */
+	public DesenvolvedorDesafio saveInscricao(DesenvolvedorDesafio inscricao) {
+		return repository.save(inscricao);
+	}
+
+	public List<DesenvolvedorDesafio> saveInscricoes(List<DesenvolvedorDesafio> inscricoes) {
+		return repository.saveAll(inscricoes);
+	}
+
+	/**
+	 * Métodos GET
+	 */
+	public List<DesenvolvedorDesafio> getInscricoes(String emailEmpresa, int idDesafio) {
+		return repository.findAllByDesafio(emailEmpresa, idDesafio);
+	}
+
+	public DesenvolvedorDesafio getInscricaoById(DesenvolvedorDesafioId id) {
+		return repository.findById(id).orElse(null);
+	}
+	/**
+	 * Método DELETE
+	 */
+	public String deleteInscricao(DesenvolvedorDesafioId id) {
+		repository.deleteById(id);
+		return "desafioHabilidade deletada: " + id;
+	}
+}
